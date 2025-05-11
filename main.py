2e6f5f68412e3c8655ab4978c26cffa656104315
Zbelli <belli.zack@gmail.com>
2025-05-11 11:11:04 -0400

    refactored to new lesson format

--- main.py
+++ main.py
@@ -1,43 +1,28 @@
-from stats import get_num_words
+import sys
+from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
 
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
-    print()
-
-    for item in chars_sorted_list:
+    for item in sorted_list:
         if not item["char"].isalpha():
             continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
+        print(f"{item['char']}: {item['num']}")
 
-    print("--- End report ---")
-
-def sort_on(d):
-    return d["num"]
-
-def chars_dict_to_sorted_list(num_chars_dict):
-    sorted_list = []
-    for ch in num_chars_dict:
-        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
-    sorted_list.sort(reverse=True, key=sort_on)
-    return sorted_list
-
-def get_chars_dict(text):
-    chars = {}
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
-    return chars
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
