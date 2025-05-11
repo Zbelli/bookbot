d47a51e052e5f1543826a08a5590a43ab62c87c4
Zbelli <belli.zack@gmail.com>
2025-05-11 09:33:05 -0400

    refactored get_num_words function to separate file: stats.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import get_num_words
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -16,15 +19,8 @@ def main():
 
     print("--- End report ---")
 
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
-
 def sort_on(d):
     return d["num"]
-
 
 def chars_dict_to_sorted_list(num_chars_dict):
     sorted_list = []
@@ -32,7 +28,6 @@ def chars_dict_to_sorted_list(num_chars_dict):
         sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
-
 
 def get_chars_dict(text):
     chars = {}
@@ -43,7 +38,6 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
-
 
 def get_book_text(path):
     with open(path) as f:
